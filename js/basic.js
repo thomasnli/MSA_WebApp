bb6b14b285983e5eba387bc320de8f91502b4685
thomasnli <911thomas.lee@gmail.com>
2016-09-15 23:58:00 +1200

    Add files via upload

--- js/basic.js
+++ js/basic.js
@@ -1,68 +1,42 @@
-var cityName;
-$(document).ready(function() {
+function myFunction() {
+    var cName = document.getElementById("search-form").value;
+    //var cName="hongkong";
+    var params = {
+        "q": cName,
+        "subscription-key": "6da4e4a5da49409ab75994a611f05ed8"
+    };
 
+    $.ajax({
+        url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?" + $.param(params),
+        beforeSend: function (xhrObj) {
+            // Request headers
+            //xhrObj.setRequestHeader("Content-Type","application/json; charset=utf-8");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "6da4e4a5da49409ab75994a611f05ed8");
+            xhrObj.getResponseHeader("Access-Control-Allow-Origin:*");
+        },
+        type: "GET",
+        // Request body
+        // data: "{body}"
+    })
+        .done(function (data) {
+            if (data.length != 0) { // if data is received
+                // Get the picture
+                var contentUrl = data.value[0].contentUrl;
+                $("html").css('background-image', 'url(contentUrl)');
 
-
-    $("#searchbtn").on("click", function () {
-        var cName=readSearchForm();
-        var url=sendPicRequest(cName);
-        changeImg(url);
-
-
-
-    });
-
-
-    function readSearchForm() {
-        //cityName=$("#search-form").val("value");
-        cityName=document.getElementById("search-form").value;
-        return cityName;
-    }
-
-
-    function sendPicRequest(cName) {
-        // var count=1; var
-
-        var parms={"q":cName, "count":1};
-        var testVar=$.param(parms);
-        $.ajax({
-            //url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=wellington&count=1"
-            url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?" + $.param(parms),
-            beforeSend: function (xhrObj) {
-                // Request headers
-                xhrObj.setRequestHeader("Content-Type", "multipart/form-data");
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "6da4e4a5da49409ab75994a611f05ed8");
-            },
-            type: "POST",
-            data: {"q":cName, "count":1}
-
-
+                callback(contentUrl);
+            } else {
+                pageheader.innerHTML = "It seems we can't get there. Try another city?";
+            }
         })
-            .done(function (data) {
-                if (data.length != 0) { // if data is received
-                    // Get the picture
-                    var contentUrl = data.value[0].contentUrl;
-                    callback(contentUrl);
-                } else {
-                    pageheader.innerHTML = "It seems we can't get there. Try another city?";
-                }
-            })
-            .fail(function (error) {
-                pageheader.innerHTML = "Sorry, something went wrong. :( Try again in a bit?";
-                console.log(error.getAllResponseHeaders());
-            });
-
-
-    }
-
-
-
-    function changeImg(url) {
-        //var picture=value.contentUrl;
-        $("html").css('background-image', 'url(url)');
-        //document.getElementById("body").style.backgroundImage="url(url)";
-    }
-});
-
-
-
+        .fail(function () {
+            alert("error");
+        });
+}
+FB.ui({
+    method: 'share_open_graph',
+    action_type: 'og.likes',
+    action_properties: JSON.stringify({
+        object:'https://developers.facebook.com/docs/',
+    })
+}, function(response){});
